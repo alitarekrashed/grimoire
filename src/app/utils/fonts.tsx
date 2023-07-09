42b8b49b7fd05ffbfba9d8cf9e8d2348470c897b
Ali Rashed <alitarekrashed@gmail.com>
2023-07-08 20:37:30 -0500

    header + fonts

---
+++ src/app/utils/fonts.tsx
@@ -0,0 +1,7 @@
+import { Inter, DM_Serif_Display } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] });
+export const dm_serif_display = DM_Serif_Display({
+  weight: "400",
+  subsets: ['latin']
+});
