ce60587fcc87cea59aee45546c99aa35ca4f7759
Ali Rashed <alitarekrashed@gmail.com>
2023-07-11 16:50:07 -0500

    add more italics

--- src/app/api/equipment/route.ts
+++ src/app/api/equipment/route.ts
@@ -96,7 +96,7 @@ export async function GET(request: Request) {
   const rhinocerousMask: Equipment = {
     name: 'rhinocerous mask',
     description:
-      'Covered with thick armor and bearing a thicker horn, a rhinoceros mask grants you increased momentum. If you Stride at least 10 feet, your next melee Strike before the end of your turn ignores the Hardness of objects with a Hardness of 5 or less. If the object has more than Hardness 5, the mask grants no benefit.    ',
+      'Covered with thick armor and bearing a thicker horn, a <i>rhinoceros mask</i> grants you increased momentum. If you Stride at least 10 feet, your next melee Strike before the end of your turn ignores the Hardness of objects with a Hardness of 5 or less. If the object has more than Hardness 5, the mask grants no benefit.    ',
     source: {
       title: 'Treasure Vault',
       page: '155',
