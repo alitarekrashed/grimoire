0a527e97e9955cb9abc618b542de0834dec35e53
Ali Rashed <alitarekrashed@gmail.com>
2023-07-13 11:58:50 -0500

    more refactoring to move towards reusable components

--- src/components/equipment-display.tsx
+++ src/components/equipment-display.tsx
@@ -1,12 +1,43 @@
 'use client'
 
 import { Equipment } from '@/models/equipment'
-import { useState } from 'react'
-import EquipmentCardList from './equipment-card-list'
-import EquipmentGrid from './equipment-grid'
+import { useEffect, useState } from 'react'
+import CardDisplayList from './card-display-list.tsx/card-display-list'
+import EquipmentCard from './equipment-card'
+import SelectableGrid from './selectable-grid/selectable-grid'
 
 export default function EquipmentDisplay() {
+  const [equipment, setEquipment] = useState([])
   const [cards, setCards] = useState<Equipment[]>([])
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      field: 'level',
+      filter: true,
+      sort: 'asc' as SortDirection,
+      maxWidth: 90,
+      flex: 1,
+    },
+    { field: 'name', filter: true, flex: 1 },
+    { field: 'category', filter: true, flex: 1 },
+    { field: 'source.title', headerName: 'Source', filter: true, flex: 1 },
+    {
+      field: 'rarity',
+      filter: true,
+      valueFormatter: (value: any): string => value.data.rarity ?? 'common',
+      flex: 1,
+      maxWidth: 150,
+    },
+  ])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/equipment', {
+      cache: 'no-store',
+    })
+      .then((result) => result.json())
+      .then((equipment) => {
+        setEquipment(equipment)
+      })
+  }, [])
 
   function handleSelectedCard(item: Equipment) {
     setCards((cards) => {
@@ -27,10 +58,20 @@ export default function EquipmentDisplay() {
   return (
     <div className="h-full grid grid-cols-5 gap-x-4">
       <div className="bg-neutral-800 p-3 rounded col-span-3 shadow-slate-200 drop-shadow-md">
-        <EquipmentGrid onSelectedItem={handleSelectedCard}></EquipmentGrid>
+        <SelectableGrid
+          rowData={equipment}
+          onSelectedItem={handleSelectedCard}
+          columnDefs={columnDefs}
+        ></SelectableGrid>
       </div>
       <div className="bg-neutral-800 p-3 rounded overflow-x-auto col-span-2 shadow-slate-200 drop-shadow-md">
-        <EquipmentCardList equipment={cards}></EquipmentCardList>
+        <CardDisplayList
+          children={cards.map((value) => (
+            <div key={value.name} className="pb-4">
+              <EquipmentCard value={value}></EquipmentCard>
+            </div>
+          ))}
+        ></CardDisplayList>
       </div>
     </div>
   )
--- src/components/selectable-grid/selectable-grid.tsx
+++ src/components/selectable-grid/selectable-grid.tsx
@@ -1,54 +1,24 @@
 'use client'
 
 import { Equipment } from '@/models/equipment'
-import { SortDirection } from 'ag-grid-community'
 import 'ag-grid-community/styles/ag-grid.css' // Core grid CSS, always needed
 import 'ag-grid-community/styles/ag-theme-alpine.css' // Optional theme CSS
 import { AgGridReact } from 'ag-grid-react' // the AG Grid React Component
-import { LegacyRef, useCallback, useEffect, useRef, useState } from 'react'
+import { LegacyRef, useCallback, useRef } from 'react'
 
-export default function EquipmentGrid({
+export default function SelectableGrid({
   onSelectedItem,
+  rowData,
+  columnDefs,
 }: {
   onSelectedItem: (item: Equipment) => void
+  rowData: any[]
+  columnDefs: any[]
 }) {
-  const [rowData, setRowData] = useState() // Set rowData to Array of Objects, one Object per Row]
-
-  const [columnDefs, setColumnDefs] = useState([
-    {
-      field: 'level',
-      filter: true,
-      sort: 'asc' as SortDirection,
-      maxWidth: 90,
-      flex: 1,
-    },
-    { field: 'name', filter: true, flex: 1 },
-    { field: 'category', filter: true, flex: 1 },
-    { field: 'source.title', headerName: 'Source', filter: true, flex: 1 },
-    {
-      field: 'rarity',
-      filter: true,
-      valueFormatter: (value: any): string => value.data.rarity ?? 'common',
-      flex: 1,
-      maxWidth: 150,
-    },
-  ])
-
   const gridRef: LegacyRef = useRef()
   const onSelectionChanged = useCallback(() => {
     const selectedRows = gridRef!.current.api.getSelectedRows()
     onSelectedItem(selectedRows[0])
-  }, [])
-
-  // Example load data from server
-  useEffect(() => {
-    fetch('http://localhost:3000/api/equipment', {
-      cache: 'no-store',
-    })
-      .then((result) => result.json())
-      .then((rowData) => {
-        setRowData(rowData)
-      })
   }, [])
 
   return (
