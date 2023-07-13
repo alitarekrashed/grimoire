33b3d6c875585c653fa9471598ae001c970da0dc
Ali Rashed <alitarekrashed@gmail.com>
2023-07-13 18:34:06 -0500

    shift title to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { dm_serif_display } from '../utils/fonts'
+import * as NavigationMenu from '@radix-ui/react-navigation-menu'
 
 export const metadata: Metadata = {
   title: 'grimoire',
@@ -15,9 +16,42 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${dm_serif_display.className} bg-gradient-to-t from-zinc-700 via-zinc-900 to-black`}
+        className={`${dm_serif_display.className} bg-gradient-to-t from-zinc-700 via-zinc-900 to-black h-screen`}
       >
-        {children}
+        <div className="h-1/6 flex justify-center ">
+          <h1 className="pt-5 mb-4 text-5xl">grimoire</h1>
+        </div>
+
+        {/* <NavigationMenu.Root>
+          <NavigationMenu.List>
+            <NavigationMenu.Item>
+              <NavigationMenu.Trigger />
+              <NavigationMenu.Content>
+                <NavigationMenu.Link />
+              </NavigationMenu.Content>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Link />
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Item>
+              <NavigationMenu.Trigger />
+              <NavigationMenu.Content>
+                <NavigationMenu.Sub>
+                  <NavigationMenu.List />
+                  <NavigationMenu.Viewport />
+                </NavigationMenu.Sub>
+              </NavigationMenu.Content>
+            </NavigationMenu.Item>
+
+            <NavigationMenu.Indicator />
+          </NavigationMenu.List>
+
+          <NavigationMenu.Viewport />
+        </NavigationMenu.Root> */}
+
+        <div className="h-5/6"> {children}</div>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@ import EquipmentDisplay from '@/components/equipment-display'
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      <div className="h-1/6 flex justify-center ">
-        <h1 className="pt-5 mb-4 text-5xl">grimoire</h1>
-      </div>
-      <div className="h-5/6">
-        <EquipmentDisplay></EquipmentDisplay>
-      </div>
+    <main className="h-full">
+      <EquipmentDisplay></EquipmentDisplay>
     </main>
   )
 }
