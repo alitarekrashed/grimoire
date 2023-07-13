e93891eb3cbdc715cfd352f62317040858929b8c
Ali Rashed <alitarekrashed@gmail.com>
2023-07-12 20:49:15 -0500

    default state whenever there are no cards selected

--- src/components/equipment-card-list.tsx
+++ src/components/equipment-card-list.tsx
@@ -1,5 +1,6 @@
 import { Equipment } from '@/models/equipment'
 import EquipmentCard from './equipment-card'
+import { roboto_serif, roboto_serif_italic } from '@/utils/fonts'
 
 export default function EquipmentCardList({
   equipment,
@@ -8,6 +9,13 @@ export default function EquipmentCardList({
 }) {
   return (
     <div className="h-full w-full">
+      {equipment.length < 1 && (
+        <span
+          className={`${roboto_serif_italic.className} flex justify-center mt-8 text-slate-400 text-sm`}
+        >
+          Try selecting an item
+        </span>
+      )}
       <div>
         {equipment.map((value) => (
           <div key={value.name} className="pb-4">
