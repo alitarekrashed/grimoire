d5118508c37d3cf3d834cf51f3a0b8d77a4782eb
Ali Rashed <alitarekrashed@gmail.com>
2023-07-14 11:56:28 -0500

    removes title for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import Navbar from '@/components/navbar/navbar'
 import type { Metadata } from 'next'
-import { dm_serif_display, roboto_serif, ysabeau } from '../utils/fonts'
+import { dm_serif_display, ysabeau } from '../utils/fonts'
 
 export const metadata: Metadata = {
   title: 'grimoire',
@@ -19,9 +19,6 @@ export default function RootLayout({
         className={`${dm_serif_display.className} bg-gradient-to-t from-stone-950 to-stone-900 min-h-screen flex wrap flex-col`}
       >
         <div className="grid grid-cols-1 pt-5 w-full">
-          <h1 className="text-4xl mb-4 flex self-center justify-self-center">
-            grimoire
-          </h1>
           <div
             className={`mb-4 ${ysabeau.className} flex self-center justify-self-center`}
           >
