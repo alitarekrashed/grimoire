4e313156e62a64773b44c9b4b28dc265db306bf2
Ali Rashed <alitarekrashed@gmail.com>
2023-07-14 15:12:25 -0500

    get rid of *-display components and pull the split views into the pages

--- src/app/reference/conditions/page.tsx
+++ src/app/reference/conditions/page.tsx
@@ -1,10 +1,34 @@
-import ConditionsDisplay from '@/components/conditions/conditions-display'
+'use client'
+
+import ConditionCard from '@/components/conditions/condition-card'
+import SplitViewDisplay from '@/components/split-view-list/split-view-list'
+import { useEffect, useState } from 'react'
 
 export default function ConditionsPage() {
+  const [conditions, setConditions] = useState([])
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'name', filter: true, flex: 1 },
+  ])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/conditions', {
+      cache: 'no-store',
+    })
+      .then((result) => result.json())
+      .then((conditions) => {
+        setConditions(conditions)
+      })
+  }, [])
+
   return (
     <div className="h-full">
-      {' '}
-      <ConditionsDisplay></ConditionsDisplay>
+      <SplitViewDisplay
+        columnDefs={columnDefs}
+        entities={conditions}
+        buildCard={(condition: Condition) => (
+          <ConditionCard value={condition}></ConditionCard>
+        )}
+      ></SplitViewDisplay>
     </div>
   )
 }
--- src/app/reference/equipment/page.tsx
+++ src/app/reference/equipment/page.tsx
@@ -1,9 +1,52 @@
-import EquipmentDisplay from '@/components/equipment/equipment-display'
+'use client'
+
+import EquipmentCard from '@/components/equipment/equipment-card'
+import SplitViewDisplay from '@/components/split-view-list/split-view-list'
+import { Equipment } from '@/models/equipment'
+import { SortDirection } from 'ag-grid-community'
+import { useEffect, useState } from 'react'
 
 export default function EquipmentPage() {
+  const [equipment, setEquipment] = useState([])
+  const [columnDefs, setColumnDefs] = useState([
+    {
+      field: 'level',
+      filter: true,
+      sort: 'asc' as SortDirection,
+      maxWidth: 90,
+      flex: 1,
+    },
+    { field: 'name', filter: true, flex: 1 },
+    { field: 'category', filter: true, flex: 1 },
+    { field: 'source.title', headerName: 'Source', filter: true, flex: 1 },
+    {
+      field: 'rarity',
+      filter: true,
+      valueFormatter: (value: any): string => value.data.rarity ?? 'common',
+      flex: 1,
+      maxWidth: 150,
+    },
+  ])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/equipment', {
+      cache: 'no-store',
+    })
+      .then((result) => result.json())
+      .then((equipment) => {
+        setEquipment(equipment)
+      })
+  }, [])
+
   return (
     <div className="h-full">
-      <EquipmentDisplay></EquipmentDisplay>
+      <SplitViewDisplay
+        columnDefs={columnDefs}
+        entities={equipment}
+        buildCard={(equipment: Equipment) => (
+          <EquipmentCard value={equipment}></EquipmentCard>
+        )}
+      ></SplitViewDisplay>
     </div>
   )
 }
