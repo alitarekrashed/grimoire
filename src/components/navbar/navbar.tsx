f956aa458e44f105654dde0843401dc9346cc193
Ali Rashed <alitarekrashed@gmail.com>
2023-07-14 14:21:38 -0500

    adds hover state for navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,15 @@
 'use client'
 import * as NavigationMenu from '@radix-ui/react-navigation-menu'
-import styles from './navbar.module.css'
 
 export default function Navbar() {
+  const navItemStyles = 'block p-2 hover:bg-stone-700 hover:rounded-lg'
+
   return (
     <NavigationMenu.Root className="text-center inline-flex justify-center relative border rounded-lg border-stone-300 bg-stone-800">
       <NavigationMenu.List className="flex justify-center ">
         <NavigationMenu.Item>
           <NavigationMenu.Link href="/reference/equipment">
-            <span className="block p-2">Equipment</span>
+            <span className={`${navItemStyles}`}>Equipment</span>
           </NavigationMenu.Link>
         </NavigationMenu.Item>
         <NavigationMenu.Item>
@@ -16,7 +17,7 @@ export default function Navbar() {
         </NavigationMenu.Item>
         <NavigationMenu.Item>
           <NavigationMenu.Link href="/reference/conditions">
-            <span className="block p-2">Conditions</span>
+            <span className={`${navItemStyles}`}>Conditions</span>
           </NavigationMenu.Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
