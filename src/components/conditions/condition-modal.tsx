2f161d22bbde2ac499b24fbfc4b2da007c819869
Ali Rashed <alitarekrashed@gmail.com>
2023-07-15 12:49:11 -0500

    adds close button with font awesome

--- src/components/conditions/condition-modal.tsx
+++ src/components/conditions/condition-modal.tsx
@@ -5,6 +5,8 @@ import * as Dialog from '@radix-ui/react-dialog'
 import styles from './condition-modal.module.css'
 
 import ConditionCard from './condition-card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 // TODO eventually the display should be clickable and allow the user to go to a page for the condition?
 // TODO maybe this should open a modal instead of a link to a new page
@@ -31,10 +33,13 @@ export default function ConditionModal({ value }: { value: Condition }) {
             </div>
             <Dialog.Close asChild>
               <button
-                className={`${styles.IconButton} text-stone-800 hover:bg-stone-400`}
+                className={`${styles.IconButton} rounded `}
                 aria-label="Close"
               >
-                X
+                <FontAwesomeIcon
+                  className="p-1.5 bg-stone-800 rounded-full hover:bg-stone-400"
+                  icon={faXmark}
+                />
               </button>
             </Dialog.Close>
           </Dialog.Content>
