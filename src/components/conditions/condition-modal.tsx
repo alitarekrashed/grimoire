80ecc4c8d28c7a5bc81659e48db9d17898950d82
Ali Rashed <alitarekrashed@gmail.com>
2023-07-15 10:26:48 -0500

    opens condition in modal

---
+++ src/components/conditions/condition-modal.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Condition from '@/models/condition'
+import * as Dialog from '@radix-ui/react-dialog'
+import styles from './condition-modal.module.css'
+
+import ConditionCard from './condition-card'
+
+// TODO eventually the display should be clickable and allow the user to go to a page for the condition?
+// TODO maybe this should open a modal instead of a link to a new page
+export default function ConditionModal({ value }: { value: Condition }) {
+  return (
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button>
+            <span className="underline" tabIndex={0}>
+              {value.name}
+            </span>
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className={styles.DialogOverlay} />
+          <Dialog.Content className={`bg-stone-800 ${styles.DialogContent}`}>
+            <Dialog.Description className={`${styles.DialogDescription}`}>
+              <ConditionCard value={value}></ConditionCard>
+            </Dialog.Description>
+            <Dialog.Close asChild>
+              <button
+                className={`${styles.IconButton} text-stone-800 hover:bg-stone-400`}
+                aria-label="Close"
+              >
+                X
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
+  )
+}
