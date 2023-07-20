4e9942e22d38745446ae2a7fa3ae9950b1d7e4b3
Ali Rashed <alitarekrashed@gmail.com>
2023-07-20 11:21:38 -0500

    adds card label list component

--- src/components/card/card-label.tsx
+++ src/components/card/card-label.tsx
@@ -18,3 +18,45 @@ export default function CardLabel({
     </span>
   )
 }
+
+export function CardLabelList({
+  fieldDefinitions,
+  labelClassName,
+  valueClassName,
+  separator,
+}: {
+  fieldDefinitions: FieldDefinition[]
+  labelClassName?: string
+  valueClassName?: string
+  separator?: 'semicolon' | 'new-line'
+}) {
+  let labels: any = []
+
+  for (let i = 0; i < fieldDefinitions.length; i++) {
+    const field = fieldDefinitions[i]
+    if (field.value) {
+      labels.push(
+        <CardLabel
+          label={field.label}
+          value={field.value}
+          labelClassName={labelClassName}
+          valueClassName={valueClassName}
+        ></CardLabel>
+      )
+      if (i < fieldDefinitions.length - 1 && fieldDefinitions[i + 1].value) {
+        if (separator === 'new-line') {
+          labels.push(<br />)
+        } else {
+          labels.push('; ')
+        }
+      }
+    }
+  }
+
+  return <>{labels}</>
+}
+
+export interface FieldDefinition {
+  label: string
+  value: string | undefined
+}
