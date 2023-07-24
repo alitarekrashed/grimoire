163043652d2a8745d8595076272852414b9d8229
Ali Rashed <alitarekrashed@gmail.com>
2023-07-24 11:33:19 -0500

    adds level and type to search result

--- src/components/search-bar/search-result.tsx
+++ src/components/search-bar/search-result.tsx
@@ -1,9 +1,47 @@
 import { EntityModel } from '@/models/entity-model'
+import { EquipmentWithVariants } from '@/models/equipment'
 
-export function SearchResult<T extends EntityModel>({ data }: { data: T }) {
+export function SearchResult({ data }: { data: EntityModel }) {
+  const level: string | undefined = buildLevelDisplay(data)
   return (
     <div className="flex justify-between">
-      <span>{data.name}</span> <span>{data.entity_type}</span>
+      <span>{data.name}</span>
+      <span>
+        {getType(data)} {level}
+      </span>
     </div>
   )
+}
+
+function getType(data: EntityModel): string {
+  switch (data.entity_type) {
+    case 'EQUIPMENT':
+    case 'EQUIPMENT_WITH_VARIANTS':
+      return 'Equipment'
+    case 'SPELL':
+      return 'Spell'
+    case 'CONDITION':
+      return 'Condition'
+    case 'TRAIT':
+      return 'Trait'
+  }
+}
+
+function buildLevelDisplay(data: EntityModel): string | undefined {
+  switch (data.entity_type) {
+    case 'EQUIPMENT':
+      return (data as EntityModel & { level: number }).level.toString()
+    case 'SPELL':
+      return (data as EntityModel & { rank: number }).rank.toString()
+    case 'EQUIPMENT_WITH_VARIANTS':
+      const levels: number[] = (data as EquipmentWithVariants).types.map(
+        (type) => type.level
+      )
+      levels.sort((a, b) => a - b)
+      let level: string = '' + levels[0]
+      level = levels.length > 1 ? level.concat('+') : level
+      return level
+    default:
+      return undefined
+  }
 }
