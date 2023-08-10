140867dc6be48e6c26cd1466b2a9834537190d19
Ali Rashed <alitarekrashed@gmail.com>
2023-08-10 15:59:02 -0500

    adds loading spinner and darkens page as it loads

--- src/components/character-builder/character-builder.tsx
+++ src/components/character-builder/character-builder.tsx
@@ -15,6 +15,7 @@ import { HeritageChoiceModal } from './heritage-choice-modal'
 import { LanguagesModal } from './languages-modal'
 import { SkillsModal } from './skills-modal'
 import { ClassFeatChoiceModal } from './class-feat.modal'
+import styles from './character-builder.module.css'
 
 export default function CharacterBuilderModal({
   playerCharacter,
@@ -108,122 +109,132 @@ export default function CharacterBuilderModal({
           </span>
         }
         body={
-          <div className="p-2">
-            <div className={`text-sm ${roboto_condensed.className}`}>
-              <div className="mb-2 inline-flex">
-                <div className="relative w-44 h-9 mr-2">
-                  <span className="text-stone-300 absolute top-0 text-[9px] pl-1.5">
-                    Name
-                  </span>
-                  <input
-                    className="absolute bottom-0 bg-transparent rounded-md h-full w-full pt-4 pl-1 border border-stone-300"
-                    value={name}
-                    onChange={(e) => {
-                      updateName(e.target.value)
-                    }}
-                  ></input>
-                </div>
-                <div className="mr-2">
-                  <AncestryChoiceModal
-                    ancestryId={character.getAncestryId()}
-                    onAncestryEdit={handleAncestryChange}
-                  ></AncestryChoiceModal>
-                </div>
-                <div className="mr-2">
-                  <HeritageChoiceModal
-                    heritageId={character.getHeritageId()}
-                    ancestry={character.getAncestry()}
-                    onHeritageChange={handleHeritageChange}
-                  ></HeritageChoiceModal>
-                </div>
-                <div className="mr-2">
-                  <BackgroundChoiceModal
-                    backgroundId={character.getBackgroundId()}
-                    onBackgroundChange={handleBackgroundChange}
-                  ></BackgroundChoiceModal>
-                </div>
-                <div className="mr-2">
-                  <AttributesModal
-                    characterEntity={character.getCharacter()}
-                    ancestry={character.getAncestry()}
-                    background={character.getBackground()}
-                    classEntity={character.getClassEntity()}
-                    onAttributeUpdate={handleAttributeChange}
-                  ></AttributesModal>
-                </div>
-                <div>
-                  <LanguagesModal
-                    onLanguagesUpdate={handleLanguageChange}
-                    character={character.getCharacter()}
-                    ancestry={character.getAncestry()}
-                  ></LanguagesModal>
-                </div>
+          <>
+            {loading && (
+              <div className={`${styles.ldsRing}`}>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
               </div>
-              <div className="inline-flex gap-2">
-                <div>
-                  {character
-                    .getLevelFeatures()
-                    .filter((sourced) => sourced.source === 'ANCESTRY')
-                    .map((value, index) => {
-                      return (
-                        <AncestryFeatChoiceModal
-                          key={`${value.source}-${index}`}
-                          existingFeat={value}
-                          existingFeatName={
-                            value.feature.value ? value.feature.value : ''
-                          }
-                          traits={character.getTraits()}
-                          onChange={handleFeatureUpdate('ANCESTRY', 'FEAT')}
-                        ></AncestryFeatChoiceModal>
-                      )
-                    })}
+            )}
+            <div className="p-2">
+              <div className={`text-sm ${roboto_condensed.className}`}>
+                <div className="mb-2 inline-flex">
+                  <div className="relative w-44 h-9 mr-2">
+                    <span className="text-stone-300 absolute top-0 text-[9px] pl-1.5">
+                      Name
+                    </span>
+                    <input
+                      className="absolute bottom-0 bg-transparent rounded-md h-full w-full pt-4 pl-1 border border-stone-300"
+                      value={name}
+                      onChange={(e) => {
+                        updateName(e.target.value)
+                      }}
+                    ></input>
+                  </div>
+                  <div className="mr-2">
+                    <AncestryChoiceModal
+                      ancestryId={character.getAncestryId()}
+                      onAncestryEdit={handleAncestryChange}
+                    ></AncestryChoiceModal>
+                  </div>
+                  <div className="mr-2">
+                    <HeritageChoiceModal
+                      heritageId={character.getHeritageId()}
+                      ancestry={character.getAncestry()}
+                      onHeritageChange={handleHeritageChange}
+                    ></HeritageChoiceModal>
+                  </div>
+                  <div className="mr-2">
+                    <BackgroundChoiceModal
+                      backgroundId={character.getBackgroundId()}
+                      onBackgroundChange={handleBackgroundChange}
+                    ></BackgroundChoiceModal>
+                  </div>
+                  <div className="mr-2">
+                    <AttributesModal
+                      characterEntity={character.getCharacter()}
+                      ancestry={character.getAncestry()}
+                      background={character.getBackground()}
+                      classEntity={character.getClassEntity()}
+                      onAttributeUpdate={handleAttributeChange}
+                    ></AttributesModal>
+                  </div>
+                  <div>
+                    <LanguagesModal
+                      onLanguagesUpdate={handleLanguageChange}
+                      character={character.getCharacter()}
+                      ancestry={character.getAncestry()}
+                    ></LanguagesModal>
+                  </div>
                 </div>
-                <div>
-                  <SkillsModal
-                    skillFeatures={character
+                <div className="inline-flex gap-2">
+                  <div>
+                    {character
+                      .getLevelFeatures()
+                      .filter((sourced) => sourced.source === 'ANCESTRY')
+                      .map((value, index) => {
+                        return (
+                          <AncestryFeatChoiceModal
+                            key={`${value.source}-${index}`}
+                            existingFeat={value}
+                            existingFeatName={
+                              value.feature.value ? value.feature.value : ''
+                            }
+                            traits={character.getTraits()}
+                            onChange={handleFeatureUpdate('ANCESTRY', 'FEAT')}
+                          ></AncestryFeatChoiceModal>
+                        )
+                      })}
+                  </div>
+                  <div>
+                    <SkillsModal
+                      skillFeatures={character
+                        .getLevelFeatures()
+                        .filter(
+                          (sourced) =>
+                            sourced.source === 'CLASS' &&
+                            sourced.feature.type === 'SKILL_SELECTION'
+                        )}
+                      // basically what we're trying to say here is "filter out Class Level 1 proficiencies when passing in existing profs"
+                      // the reasoning is that since all the values for Class Level 1 profs are encapsulated within this modal, it can just
+                      // check itself for its values
+                      proficiencies={character.getSkills('1')}
+                      onSkillsUpdate={handleFeatureUpdate(
+                        'CLASS',
+                        'SKILL_SELECTION'
+                      )}
+                    ></SkillsModal>
+                  </div>
+                  <div>
+                    {character
                       .getLevelFeatures()
                       .filter(
                         (sourced) =>
                           sourced.source === 'CLASS' &&
-                          sourced.feature.type === 'SKILL_SELECTION'
-                      )}
-                    // basically what we're trying to say here is "filter out Class Level 1 proficiencies when passing in existing profs"
-                    // the reasoning is that since all the values for Class Level 1 profs are encapsulated within this modal, it can just
-                    // check itself for its values
-                    proficiencies={character.getSkills('1')}
-                    onSkillsUpdate={handleFeatureUpdate(
-                      'CLASS',
-                      'SKILL_SELECTION'
-                    )}
-                  ></SkillsModal>
-                </div>
-                <div>
-                  {character
-                    .getLevelFeatures()
-                    .filter(
-                      (sourced) =>
-                        sourced.source === 'CLASS' &&
-                        sourced.feature.type === 'CLASS_FEAT_SELECTION'
-                    )
-                    .map((value, index) => {
-                      return (
-                        <ClassFeatChoiceModal
-                          key={`${value.source}-${index}`}
-                          existingFeat={value}
-                          traits={[
-                            character.getClassEntity().name.toLowerCase(),
-                          ]}
-                          onChange={handleFeatureUpdate(
-                            'CLASS',
-                            'CLASS_FEAT_SELECTION'
-                          )}
-                        ></ClassFeatChoiceModal>
+                          sourced.feature.type === 'CLASS_FEAT_SELECTION'
                       )
-                    })}
+                      .map((value, index) => {
+                        return (
+                          <ClassFeatChoiceModal
+                            key={`${value.source}-${index}`}
+                            existingFeat={value}
+                            traits={[
+                              character.getClassEntity().name.toLowerCase(),
+                            ]}
+                            onChange={handleFeatureUpdate(
+                              'CLASS',
+                              'CLASS_FEAT_SELECTION'
+                            )}
+                          ></ClassFeatChoiceModal>
+                        )
+                      })}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </>
         }
         closeButtons={[
           {
