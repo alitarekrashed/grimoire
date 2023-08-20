e89de6b0fed5db913deb1bad230b1b71ed56a76f
Ali Rashed <alitarekrashed@gmail.com>
2023-08-20 13:14:37 -0500

    rename skill select

--- src/components/character-builder/skills/multiple-skill-select.tsx
+++ src/components/character-builder/skills/multiple-skill-select.tsx
@@ -58,7 +58,7 @@ export function MultipleSkillSelect({
       className="border border-stone-300 rounded-md relative flex w-44 h-9 p-1 justify-center items-center hover:bg-stone-600"
       tabIndex={0}
     >
-      {name ? name : 'Initial Skills'} {setCount}/{totalCount}
+      {name ? name : 'Trained Skills'} {setCount}/{totalCount}
     </button>
   )
 
