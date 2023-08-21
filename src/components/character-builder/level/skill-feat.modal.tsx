162820ef2dd85a7e97e97fae97764db1e9322584
Ali Rashed <alitarekrashed@gmail.com>
2023-08-21 13:20:53 -0500

    implemenets skill prereqs

--- src/components/character-builder/level/skill-feat.modal.tsx
+++ src/components/character-builder/level/skill-feat.modal.tsx
@@ -1,10 +1,14 @@
-import { Feat } from '@/models/db/feat'
+import { Feat, Prerequisite } from '@/models/db/feat'
 import { SourcedFeature } from '@/models/player-character'
 import { cloneDeep } from 'lodash'
 import { useContext, useEffect, useState } from 'react'
 import { PlayerCharacterContext } from '../../character-display/player-character-context'
 import { CharacterLevelContext } from '../character-level-context'
 import { FeatureChoiceModal } from '../feature-choice-modal'
+import {
+  getGreaterThan,
+  isGreaterThanOrEqualTo,
+} from '@/utils/services/gear-proficiency-manager'
 
 export function SkillFeatChoiceModal({
   existingFeat,
@@ -28,7 +32,28 @@ export function SkillFeatChoiceModal({
     })
       .then((result) => result.json())
       .then((feats) => {
-        let filtered = feats.filter((feat: Feat) => feat.level <= level)
+        let filtered = feats
+          .filter((feat: Feat) => feat.level <= level)
+          .filter((feat: Feat) => {
+            if (feat.prerequisites) {
+              return feat.prerequisites.every((prerequisite: Prerequisite) => {
+                if (prerequisite.type === 'SKILL') {
+                  console.log(prerequisite)
+                  console.log(
+                    playerCharacter.getSkills().get(prerequisite.value.skill)!
+                      .rank
+                  )
+                  return isGreaterThanOrEqualTo(
+                    playerCharacter.getSkills().get(prerequisite.value.skill)!
+                      .rank,
+                    prerequisite.value.minimum_rank
+                  )
+                }
+                return true
+              })
+            }
+            return true
+          })
         setFeats(filtered)
       })
   }, [playerCharacter.getTraits()])
