ad853183624b63ea783d0a149dc0052b87fd0df5
Ali Rashed <alitarekrashed@gmail.com>
2023-08-21 15:22:28 -0500

    adds generic feat choice modal

--- src/components/character-builder/level/feat-choice-modal.tsx
+++ src/components/character-builder/level/feat-choice-modal.tsx
@@ -1,61 +1,97 @@
-import { Feat, Prerequisite } from '@/models/db/feat'
+import { Feat } from '@/models/db/feat'
 import { SourcedFeature } from '@/models/player-character'
-import { cloneDeep } from 'lodash'
 import { useContext, useEffect, useState } from 'react'
-import { PlayerCharacterContext } from '../../character-display/player-character-context'
-import { CharacterLevelContext } from '../character-level-context'
 import { FeatureChoiceModal } from '../feature-choice-modal'
-import {
-  getGreaterThan,
-  isGreaterThanOrEqualTo,
-} from '@/utils/services/gear-proficiency-manager'
-import { CalculatedProficiency, SkillType } from '@/models/statistic'
+import { cloneDeep } from 'lodash'
+import { PlayerCharacterContext } from '../../character-display/player-character-context'
+import { FeatSubChoiceModal } from './feat-subchoice-modal'
+import { CharacterLevelContext } from '../character-level-context'
+import { isGreaterThanOrEqualTo } from '@/utils/services/gear-proficiency-manager'
 
-export function SkillFeatChoiceModal({
+export function FeatChoiceModal({
+  name,
+  traits,
   existingFeat,
   onChange,
 }: {
+  name: string
+  traits: string[]
   existingFeat: SourcedFeature
   onChange: (sourcedFeature: SourcedFeature[]) => void
 }) {
   const { playerCharacter } = useContext(PlayerCharacterContext)
   const { level } = useContext(CharacterLevelContext)
-  const [skillFeat, setSkillFeat] = useState<SourcedFeature>(existingFeat)
+  const [feat, setFeat] = useState<SourcedFeature>(existingFeat)
+  const [featWithSubChoice, setFeatWithSubChoice] = useState<Feat>()
   const [feats, setFeats] = useState<Feat[]>([])
 
+  const matchFeat = () => {
+    const matched = feats.find((val) => val.name === feat.feature.value)
+    if (matched && matched.configuration) {
+      setFeatWithSubChoice(matched)
+    } else {
+      setFeatWithSubChoice(undefined)
+    }
+  }
+
   useEffect(() => {
-    setSkillFeat(existingFeat)
+    setFeat(existingFeat)
   }, [existingFeat])
 
   useEffect(() => {
-    fetch(`http://localhost:3000/api/feats?traits=skill`, {
+    fetch(`http://localhost:3000/api/feats?traits=${traits}`, {
       cache: 'no-store',
     })
       .then((result) => result.json())
       .then((feats) => {
-        setFeats(filterFeats(feats, level, playerCharacter.getSkills()))
+        let filtered: Feat[] = filterFeats(
+          feats,
+          level,
+          playerCharacter.getSkills()
+        )
+        filtered.sort((a, b) => b.level - a.level)
+        setFeats(filtered)
       })
   }, [playerCharacter.getTraits()])
+
+  useEffect(() => {
+    matchFeat()
+  }, [feat, feats])
+
+  const handleSubChoiceChange = (value: string) => {
+    const updated = cloneDeep(feat)
+    updated.feature.context = [value]
+    onChange([updated])
+  }
 
   return (
     <>
       <FeatureChoiceModal
-        label="Skill Feat"
+        label={`${name} Feat`}
         entities={feats}
         initialId={existingFeat.feature.value ?? ''}
         idField="name"
-        onSave={(feat: Feat) => {
-          const updated = cloneDeep(skillFeat)
-          updated.feature.value = feat.name
+        onSave={(val: Feat) => {
+          const updated = cloneDeep(feat)
+          updated.feature.value = val.name
           updated.feature.context = []
           onChange([updated])
         }}
         onClear={() => {
-          const updated = cloneDeep(skillFeat)
+          const updated = cloneDeep(feat)
           updated.feature.value = null!
           onChange([updated])
         }}
       ></FeatureChoiceModal>
+      {featWithSubChoice && (
+        <div className="mt-1">
+          <FeatSubChoiceModal
+            feat={featWithSubChoice}
+            choice={feat.feature.context![0]}
+            onChange={handleSubChoiceChange}
+          ></FeatSubChoiceModal>
+        </div>
+      )}
     </>
   )
 }
