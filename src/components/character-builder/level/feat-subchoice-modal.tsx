d21a3b693661cf7752c171542f330ee6fffd6c72
Ali Rashed <alitarekrashed@gmail.com>
2023-08-24 12:25:35 -0500

    adds other input component (#190)

--- src/components/character-builder/level/feat-subchoice-modal.tsx
+++ src/components/character-builder/level/feat-subchoice-modal.tsx
@@ -3,6 +3,8 @@ import { ChoiceSelect } from '../../choice-select/choice-select'
 import { useState } from 'react'
 import { useDebounce } from '@/utils/debounce'
 import { BlockIndicator } from '@/components/indicators/indicator'
+import { title } from 'process'
+import { OtherInput } from '@/components/choice-select/other-input'
 
 export function FeatSubChoiceModal({
   feat,
@@ -37,26 +39,14 @@ export function FeatSubChoiceModal({
         }}
       ></ChoiceSelect>
       {otherValue !== undefined && (
-        <div className="mt-1 flex flex-col gap-1 w-full">
-          <div className="relative h-9">
-            <span className="text-stone-300 absolute top-0 text-[9px] pl-1.5">
-              Other: {feat.configuration!.title}
-            </span>
-            <input
-              className="absolute bottom-0 bg-transparent rounded-md h-full w-full pt-4 pl-1 border border-stone-300"
-              value={otherValue}
-              onChange={(e) => {
-                setOtherValue(e.target.value)
-                debouncedOnChange()
-              }}
-            ></input>
-          </div>
-          {!otherValue && (
-            <BlockIndicator
-              message={`Please select a ${feat.configuration!.title}`}
-            />
-          )}
-        </div>
+        <OtherInput
+          value={otherValue}
+          onChange={(val) => {
+            setOtherValue(val)
+            debouncedOnChange()
+          }}
+          title={feat.configuration!.title}
+        ></OtherInput>
       )}
     </>
   )
