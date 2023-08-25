d766c7b9bd984fbdf00441e6a5e1f4d6a26be3f5
Ali Rashed <alitarekrashed@gmail.com>
2023-08-25 15:26:16 -0500

    some cleanup

--- src/utils/services/feat-manager.ts
+++ src/utils/services/feat-manager.ts
@@ -26,87 +26,80 @@ export class FeatManager {
   }
 
   static async build(feats: Feature[]) {
-    const featNames: string[] = []
-
-    const resolveFeats = async (
-      feats: Feature[]
-    ): Promise<SourcedFeature[]> => {
-      let resolvedFeatures: SourcedFeature[] = []
-      const resolvedFeats: {
-        context: string[] | undefined
-        feat: Feat
-      }[] = await Promise.all(
-        feats
-          .filter((feat) => feat?.value)
-          .map((feat) => getAndConvertFeat(feat))
-      )
-
-      let additionalFeats: Feature[] = []
-      resolvedFeats
-        .filter((val) => val)
-        .forEach((featWithContext: FeatWithContext) => {
-          resolvedFeatures.push(
-            ...featWithContext.feat.features
-              .filter((feature) => !feature.value.action)
-              .filter((feature) => feature.type !== 'FEAT')
-              .map((feature: Feature) => {
-                let modifiedFeature = cloneDeep(feature)
-                if (
-                  feature.type === 'MISC' &&
-                  featWithContext.context &&
-                  feature.value.description
-                ) {
-                  featWithContext.context.forEach((val, index) => {
-                    modifiedFeature.value.description =
-                      modifiedFeature.value.description.replaceAll(
-                        `{${index}}`,
-                        val
-                      )
-                  })
-                  modifiedFeature.context = featWithContext.context
-                }
-                featNames.push(featWithContext.feat.name)
-                return {
-                  source: featWithContext.feat.name,
-                  feature: modifiedFeature,
-                }
-              })
-          )
-          if (featWithContext.feat.activation) {
-            featNames.push(featWithContext.feat.name)
-            resolvedFeatures.push({
-              source: featWithContext.feat.name,
-              feature: { type: 'ACTION', value: featWithContext.feat },
-            })
-          }
-          additionalFeats.push(
-            ...featWithContext.feat.features.filter(
-              (feature) => feature.type === 'FEAT'
-            )
-          )
-        })
-
-      if (additionalFeats.length > 0) {
-        resolvedFeatures.push(...(await resolveFeats(additionalFeats)))
-      }
-
-      return resolvedFeatures
-    }
-
     const resolved = await resolveFeats(feats)
+    const featNames: string[] = resolved.map(
+      (sourced: SourcedFeature) => sourced.source
+    )
     return new FeatManager(resolved, featNames)
   }
 }
 
-export async function getAndConvertFeat(
-  feature: Feature
-): Promise<FeatWithContext> {
+async function resolveFeats(feats: Feature[]): Promise<SourcedFeature[]> {
+  let resolvedFeatures: SourcedFeature[] = []
+  const resolvedFeats: {
+    context: string[] | undefined
+    feat: Feat
+  }[] = await Promise.all(
+    feats.filter((feat) => feat?.value).map((feat) => getAndConvertFeat(feat))
+  )
+
+  let additionalFeats: Feature[] = []
+  resolvedFeats
+    .filter((val) => val)
+    .forEach((featWithContext: FeatWithContext) => {
+      resolvedFeatures.push(
+        ...featWithContext.feat.features
+          .filter((feature) => !feature.value.action)
+          .filter((feature) => feature.type !== 'FEAT')
+          .map((feature: Feature) => {
+            let modifiedFeature = cloneDeep(feature)
+            if (
+              feature.type === 'MISC' &&
+              featWithContext.context &&
+              feature.value.description
+            ) {
+              featWithContext.context.forEach((val, index) => {
+                modifiedFeature.value.description =
+                  modifiedFeature.value.description.replaceAll(
+                    `{${index}}`,
+                    val
+                  )
+              })
+              modifiedFeature.context = featWithContext.context
+            }
+            return {
+              source: featWithContext.feat.name,
+              feature: modifiedFeature,
+            }
+          })
+      )
+      if (featWithContext.feat.activation) {
+        resolvedFeatures.push({
+          source: featWithContext.feat.name,
+          feature: { type: 'ACTION', value: featWithContext.feat },
+        })
+      }
+      additionalFeats.push(
+        ...featWithContext.feat.features.filter(
+          (feature) => feature.type === 'FEAT'
+        )
+      )
+    })
+
+  if (additionalFeats.length > 0) {
+    resolvedFeatures.push(...(await resolveFeats(additionalFeats)))
+  }
+
+  return resolvedFeatures
+}
+
+async function getAndConvertFeat(feature: Feature): Promise<FeatWithContext> {
   const feats = (await getFeat(feature.value)) as Feat[]
   const feat = feats.length > 0 ? feats[0] : undefined!
   return { context: feature.context, feat: feat }
 }
 
-export async function getFeat(name: string) {
+async function getFeat(name: string) {
   return name
     ? await (
         await fetch(`http://localhost:3000/api/feats?name=${name}`, {
