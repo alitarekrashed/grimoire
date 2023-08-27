de4423d9d56f498a1c68de9ed031d0128ba67682
Ali Rashed <alitarekrashed@gmail.com>
2023-08-27 00:11:30 -0500

    add focus points

--- src/components/character-display/focus-spells.tsx
+++ src/components/character-display/focus-spells.tsx
@@ -1,10 +1,27 @@
 import { Spell } from '@/models/db/spell'
 import { SpellInlineDisplay } from '../spells/spell-inline-display'
+import { SiCodemagic } from 'react-icons/si'
 
 export function FocusSpells({ spells }: { spells: Spell[] }) {
+  const focusPool: number = Math.min(spells.length, 3)
+  const options = []
+
+  for (let i = 0; i < focusPool; i++) {
+    options.push({ checked: false })
+  }
+
   return (
     <div>
-      <div className="text-lg font-light">Focus</div>
+      <div className="text-lg font-light flex flex-row gap-2 items-center">
+        Focus
+        <div className="flex flex-row gap-1">
+          {options.map((value, index) => (
+            <button key={index} className="hover:text-rose-600">
+              <SiCodemagic />
+            </button>
+          ))}
+        </div>
+      </div>
       <span className="text-xs">
         {spells &&
           spells.length > 0 &&
