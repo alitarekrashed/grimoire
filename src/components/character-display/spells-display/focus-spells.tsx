1d5369ae4dbb2f7aa0f78371b79459d870e8b097
Ali Rashed <alitarekrashed@gmail.com>
2023-08-27 10:44:53 -0500

    persist state for focus point usage

--- src/components/character-display/spells-display/focus-spells.tsx
+++ src/components/character-display/spells-display/focus-spells.tsx
@@ -1,13 +1,38 @@
 import { Spell } from '@/models/db/spell'
+import { useContext } from 'react'
 import { SpellInlineDisplay } from '../../spells/spell-inline-display'
+import { PlayerCharacterContext } from '../player-character-context'
 import { SpellSlot } from './spell-slot'
+import { cloneDeep } from 'lodash'
+import { PlayerCharacter } from '@/models/player-character'
 
 export function FocusSpells({ spells }: { spells: Spell[] }) {
-  const focusPool: number = Math.min(spells.length, 3)
-  const options: { checked: boolean }[] = []
+  const {
+    playerCharacter,
+    updatePlayerCharacter,
+    updateAndSavePlayerCharacter,
+  } = useContext(PlayerCharacterContext)
 
-  for (let i = 0; i < focusPool; i++) {
-    options.push({ checked: false })
+  const focusPool: number = Math.min(spells.length, 3)
+
+  const currentNumber =
+    playerCharacter.getCharacter().player_state.focus_points.length
+
+  const updated = cloneDeep(playerCharacter.getCharacter())
+
+  if (currentNumber < focusPool) {
+    const toAdd = focusPool - currentNumber
+    for (let i = 0; i < toAdd; i++) {
+      updated.player_state.focus_points.push(false)
+    }
+    PlayerCharacter.build(updated).then((val) => updatePlayerCharacter(val))
+  } else if (currentNumber > focusPool) {
+    let removalCounter = currentNumber - focusPool
+    updated.player_state.focus_points.splice(
+      updated.player_state.focus_points.length - removalCounter,
+      removalCounter
+    )
+    PlayerCharacter.build(updated).then((val) => updatePlayerCharacter(val))
   }
 
   return (
@@ -15,15 +40,25 @@ export function FocusSpells({ spells }: { spells: Spell[] }) {
       <div className="text-lg font-light flex flex-row gap-2 items-center">
         Focus
         <div className="flex flex-row gap-1">
-          {options.map((value, index) => (
-            <SpellSlot
-              key={index}
-              initial={options[index].checked}
-              onClick={(checked: boolean) => {
-                options[index].checked = checked
-              }}
-            ></SpellSlot>
-          ))}
+          {playerCharacter
+            .getCharacter()
+            .player_state.focus_points.map((value, index) => (
+              <SpellSlot
+                key={index}
+                initial={
+                  playerCharacter.getCharacter().player_state.focus_points[
+                    index
+                  ]
+                }
+                onClick={(checked: boolean) => {
+                  const updated = cloneDeep(playerCharacter.getCharacter())
+                  updated.player_state.focus_points[index] = checked
+                  PlayerCharacter.build(updated).then((val) =>
+                    updateAndSavePlayerCharacter(val)
+                  )
+                }}
+              ></SpellSlot>
+            ))}
         </div>
       </div>
       <span className="text-xs">
