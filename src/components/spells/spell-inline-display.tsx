2e8d7b6cbb428f63cff3468e348c7fd874931930
Ali Rashed <alitarekrashed@gmail.com>
2023-08-28 00:38:28 -0500

    adds spell level to the character sheet (#212)

--- src/components/spells/spell-inline-display.tsx
+++ src/components/spells/spell-inline-display.tsx
@@ -1,11 +1,12 @@
 import { Spell } from '@/models/db/spell'
 import * as Collapsible from '@radix-ui/react-collapsible'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { ActionRenderer } from '../activation-displays/action-renderer'
 import { SavingThrowDisplay } from '../activation-displays/activation-description'
 import { TraitsList } from '../card/traits-list'
 import { ParsedDescription } from '../parsed-description/parsed-description'
 import { Button } from '../base/button'
+import { PlayerCharacterContext } from '../character-display/player-character-context'
 
 export function SpellInlineDisplay({
   spell,
@@ -16,6 +17,7 @@ export function SpellInlineDisplay({
   castDisabled?: boolean
   onCast: () => void
 }) {
+  const { playerCharacter } = useContext(PlayerCharacterContext)
   const [disabled, setDisabled] = useState<boolean>(false)
 
   useEffect(() => {
@@ -34,11 +36,19 @@ export function SpellInlineDisplay({
           <div className="flex-1 ml-12">
             <Collapsible.Root defaultOpen={false}>
               <Collapsible.Trigger className="w-full py-0.5 flex justify-start rounded-sm bg-stone-300/40 hover:bg-stone-500/40">
-                <span className="ml-1 mr-1">{spell.name}</span>
-                <ActionRenderer
-                  activation={spell.activation}
-                  size={14}
-                ></ActionRenderer>
+                <span className="flex-0 ml-1 mr-1">{spell.name}</span>
+                <span className="flex-1 text-start">
+                  <ActionRenderer
+                    activation={spell.activation}
+                    size={14}
+                  ></ActionRenderer>
+                </span>
+                <span className="font-bold flex-0 mr-2">
+                  <span className="mr-1">{getType(spell)}</span>
+                  {spell.focus || spell.rank === 0
+                    ? Math.ceil(playerCharacter.getCharacter().level / 2)
+                    : spell.rank}
+                </span>
               </Collapsible.Trigger>
               <Collapsible.Content className="overflow-hidden">
                 <div className="my-1">
@@ -72,4 +82,11 @@ export function SpellInlineDisplay({
       )}
     </>
   )
+}
+
+function getType(spell: Spell) {
+  if (spell.focus) {
+    return 'Focus'
+  }
+  return spell.rank === 0 ? 'Cantrip' : 'Spell'
 }
