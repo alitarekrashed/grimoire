542e3744696951718cd84651371ed5a1d3fc6083
Ali Rashed <alitarekrashed@gmail.com>
2023-09-25 11:15:31 -0500

    improve inline-action display (#259)
    display trigger
    
    display requirements
    
    recator
    
    display all requirements
    
    display traits
    
    sort actions by num_actions
    
    style improvements
    
    add effect header
    
    actions sorted alphabetically
    
    adds chevron for expansion

--- src/components/actions/action-inline-display.tsx
+++ src/components/actions/action-inline-display.tsx
@@ -1,82 +1,69 @@
 import { Action } from '@/models/db/action'
-import { EntityModel } from '@/models/db/entity-model'
-import { Feat } from '@/models/db/feat'
-import { retrieveEntity } from '@/utils/services/reference-lookup.service'
 import * as Collapsible from '@radix-ui/react-collapsible'
-import { useContext, useEffect, useState } from 'react'
+import { cloneDeep } from 'lodash'
+import { ReactNode, useContext, useEffect, useState } from 'react'
 import { ActionRenderer } from '../activation-displays/action-renderer'
 import { SavingThrowDisplay } from '../activation-displays/activation-description'
-import { ParsedDescription } from '../parsed-description/parsed-description'
-import { PlayerCharacter } from '@/models/player-character'
+import { Separator } from '../base/separator'
+import { TraitsList } from '../card/traits-list'
 import { PlayerCharacterContext } from '../character-display/player-character-context'
+import { ParsedDescription } from '../parsed-description/parsed-description'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
-export function ActionInlineDisplay({
-  actionName,
-}: {
-  actionName: string | Feat
-}) {
+export function ActionInlineDisplay({ initial }: { initial: Action }) {
   const { playerCharacter } = useContext(PlayerCharacterContext)
   const [action, setAction] = useState<Action>()
+  const [expanded, setExpanded] = useState<boolean>(false)
 
-  const getAction = async (actionName: string | Feat) => {
-    let action: Action = undefined!
-    if (typeof actionName === 'string') {
-      action = (await retrieveEntity(actionName, 'ACTION')) as Action
-    } else {
-      action = {
-        description: actionName.description,
-        _id: actionName._id,
-        activation: actionName.activation!,
-        name: actionName.name.toLowerCase(),
-        source: actionName.source,
-        saving_throw: actionName.saving_throw,
-        entity_type: 'ACTION',
-      }
-    }
-    return action
-  }
+  const additional: ReactNode[] = getAdditional(action)
 
   useEffect(() => {
-    getAction(actionName).then((action) => {
-      let description = action.description
-      playerCharacter
-        .getFeatModifications(action.name.toLowerCase())
-        .forEach((val) => {
-          description = description.concat(
-            `<br/><br/><b>${val.name}</b><br/>${val.value}`
-          )
-        })
-      action.description = description
-      setAction(action)
-    })
-  }, [actionName])
+    const value = cloneDeep(initial)
+    let description = value.description
+    playerCharacter
+      .getFeatModifications(value.name.toLowerCase())
+      .forEach((val) => {
+        description = description.concat(
+          `<br/><br/><i>${val.name}</i><br/>${val.value}`
+        )
+      })
+    value.description = description
+    setAction(value)
+  }, [initial])
 
   return (
     <>
       {action && (
-        <Collapsible.Root defaultOpen={false}>
-          <Collapsible.Trigger className="w-full flex justify-start rounded-sm bg-stone-300/40 hover:bg-stone-500/40">
-            <span className="ml-1 mr-1">{action.name}</span>
-            <ActionRenderer
-              activation={action.activation}
-              size={14}
-            ></ActionRenderer>
+        <Collapsible.Root defaultOpen={false} onOpenChange={setExpanded}>
+          <Collapsible.Trigger className="w-full flex flex-col pl-1 pb-1 justify-start rounded-sm bg-stone-300/40 hover:bg-stone-500/40">
+            <div className="w-full h-full flex flex-row items-center">
+              <div className="flex-1">
+                <div className="float-left">
+                  <span className="mr-1">{action.name}</span>
+                  <ActionRenderer
+                    activation={action.activation}
+                    size={14}
+                  ></ActionRenderer>
+                </div>
+              </div>
+              <div className="pr-1">
+                {expanded ? <FaChevronUp /> : <FaChevronDown />}
+              </div>
+            </div>
+            <TraitsList traits={action.traits ?? []}></TraitsList>
+            {additional.length > 0 && <Separator className="w-full my-0.5" />}
+            {additional.length > 0 && (
+              <div className="text-left flex flex-col w-full">{additional}</div>
+            )}
           </Collapsible.Trigger>
           <Collapsible.Content className="overflow-hidden">
-            {action.activation.trigger && (
-              <div className="ml-1 mb-1">
-                <span className="font-semibold">Trigger </span>
-                <ParsedDescription
-                  description={action.activation.trigger}
-                ></ParsedDescription>
-              </div>
-            )}
-            <div className="ml-1">
+            <div className="pl-1 pt-1 rounded-b-sm bg-stone-500/40">
               <ParsedDescription
                 description={action.description}
               ></ParsedDescription>
               {action.saving_throw && (
-                <div className="mt-1">
+                <div className="mt-2">
+                  <span className="font-semibold">Effect</span>
                   <SavingThrowDisplay
                     value={action.saving_throw}
                   ></SavingThrowDisplay>
@@ -88,4 +75,32 @@ export function ActionInlineDisplay({
       )}
     </>
   )
+}
+
+function getAdditional(action: Action | undefined) {
+  const additional = []
+
+  if (action && action.activation.requirements) {
+    additional.push(
+      <div>
+        <span className="font-semibold">Requirements </span>
+        <ParsedDescription
+          description={action.activation.requirements}
+        ></ParsedDescription>
+      </div>
+    )
+  }
+
+  if (action && action.activation.trigger) {
+    additional.push(
+      <div>
+        <span className="font-semibold">Trigger </span>
+        <ParsedDescription
+          description={action.activation.trigger}
+        ></ParsedDescription>
+      </div>
+    )
+  }
+
+  return additional
 }
