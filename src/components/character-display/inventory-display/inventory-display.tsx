0eccc69e69c812a8fdbe090b20cb144584e09704
Ali Rashed <alitarekrashed@gmail.com>
2023-10-30 15:15:12 -0500

    extract bulk display component

--- src/components/character-display/inventory-display/inventory-display.tsx
+++ src/components/character-display/inventory-display/inventory-display.tsx
@@ -1,47 +1,21 @@
 import { Separator } from '@/components/base/separator'
 import { TraitsList } from '@/components/card/traits-list'
 import { CharacterEquipment } from '@/models/db/character-entity'
-import { EquipmentCategory } from '@/models/db/equipment'
 import { useContext } from 'react'
-import MoneyDisplay from '../money-display/money-display'
 import { PlayerCharacterContext } from '../player-character-context'
+import MoneyDisplay from './money-display/money-display'
+import BulkDisplay from './bulk-display'
 
 export default function InventoryDisplay() {
   const { playerCharacter } = useContext(PlayerCharacterContext)
-
-  const encumberanceLimit =
-    Math.max(0, playerCharacter.getAttributes().Strength) + 5
-  const maxBulk = Math.max(0, playerCharacter.getAttributes().Strength) + 10
-  const totalLightItems = playerCharacter
-    .getCharacter()
-    .equipment.filter((value) => value.item.bulk === 'L').length
-  const calculatedBulk =
-    playerCharacter
-      .getCharacter()
-      .equipment.filter((value) => value.item.bulk && value.item.bulk !== 'L')
-      .map((value) => Number(value.item.bulk))
-      .reduce((sum, value) => sum + value, 0) + Math.floor(totalLightItems / 10)
 
   return (
     <>
       <div className="flex flex-row w-full items-center">
         <div className="flex-1">
-          <div className="flex flex-col w-fit items-start text-xs py-1 px-1 border border-stone-500 rounded">
-            <div className="flex flex-row text-sm justify-center w-full">
-              <span className="mr-2">Bulk</span>
-              <span>{calculatedBulk}</span>
-            </div>
-            <div className="flex flex-row font-extralight w-full text-[10px]">
-              <span className="font-light flex-1 pr-2">Encumbered</span>
-              <span>{encumberanceLimit}+</span>
-            </div>
-            <div className="flex flex-row font-extralight w-full text-[10px]">
-              <span className="font-light flex-1 pr-2">Max</span>
-              <span>{maxBulk}+</span>
-            </div>
-          </div>
+          <BulkDisplay></BulkDisplay>
         </div>
-        <div className="">
+        <div>
           <MoneyDisplay></MoneyDisplay>
         </div>
       </div>
